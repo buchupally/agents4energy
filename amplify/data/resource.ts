c6ce6e03561da87a1338b2ef7b5f34b94fb6ecbb
Krishna Buchupally <buchupally@gmail.com>
2025-04-06 15:45:24 -0500

    Revert "use Sonnet v2 models"
    This reverts commit d884ac32a53da535f19a0e4036030ffd9e3640cc.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -27,7 +27,7 @@ export const productionAgentFunction = defineFunction({
     // STRUCTURED_OUTPUT_MODEL_ID: 'amazon.nova-lite-v1:0',
     // STRUCTURED_OUTPUT_MODEL_ID: 'us.anthropic.claude-3-haiku-20240307-v1:0',
     STRUCTURED_OUTPUT_MODEL_ID: 'us.anthropic.claude-3-5-haiku-20241022-v1:0',
-    MODEL_ID: 'us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+    MODEL_ID: 'us.anthropic.claude-3-5-sonnet-20240620-v1:0',
     // MODEL_ID: 'us.anthropic.claude-3-5-haiku-20241022-v1:0',
     // MODEL_ID: 'us.anthropic.claude-3-sonnet-20240229-v1:0',
     // MODEL_ID: 'us.anthropic.claude-3-haiku-20240307-v1:0',
@@ -43,7 +43,7 @@ export const planAndExecuteAgentFunction = defineFunction({
   entry: '../functions/planAndExecuteAgent/index.ts',
   timeoutSeconds: 900,
   environment: {
-    MODEL_ID: 'us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+    MODEL_ID: 'us.anthropic.claude-3-5-sonnet-20240620-v1:0'
     // MODEL_ID: 'us.anthropic.claude-3-5-haiku-20241022-v1:0'
     // MODEL_ID: 'us.anthropic.claude-3-sonnet-20240229-v1:0'
     // MODEL_ID: 'us.anthropic.claude-3-haiku-20240307-v1:0'
